c23ae8128f73874a607a2a31def44ea8c0c627e6
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-24 18:37:34 -0800

    Refactor routes, controllers, services, directives into separate files

---
+++ controllers.js
@@ -0,0 +1,31 @@
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  });
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.days = $routeParams.days || '3';
+
+  $scope.weatherAPI = $resource(weather.API_URL, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
+
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appid: weather.API_KEY });
+
+  console.log($scope.weatherResult);
+
+  $scope.convertToFahrenheit = function(degK) {
+    return Math.round((1.8 * (degK - 273)) + 32);
+  };
+
+  $scope.convertToDate = function(date) {
+    return new Date(date * 1000);
+  };
+
+}]);
