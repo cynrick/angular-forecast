bb158fe45f6ea70514620406bc4fe48b90a5f6fd
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-24 17:00:24 -0800

    Retrieve forecast data via forecastController

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ weatherApp.config(function($routeProvider) {
   .when('/forecast', {
     templateUrl: 'pages/forecast.html',
     controller: 'forecastController'
+  })
+
+  .when('/forecast/:days', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
   });
 
 });
@@ -32,8 +37,24 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
   $scope.city = cityService.city;
+
+  $scope.days = $routeParams.days || 5;
+
+  $scope.weatherAPI = $resource(weather.API_URL, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
+
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appid: weather.API_KEY });
+
+  console.log($scope.weatherResult);
+
+  $scope.convertToFahrenheit = function(degK) {
+    return Math.round((1.8 * (degK - 273)) + 32);
+  };
+
+  $scope.convertToDate = function(date) {
+    return new Date(date * 1000);
+  };
 
 }]);
